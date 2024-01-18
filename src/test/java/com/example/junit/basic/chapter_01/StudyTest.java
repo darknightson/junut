580815aae33c6af4d812f8b1e80b6835e544403a
anthony.son <anthony.son@kakaoent.com>
2024-01-18 23:31:54 +0900

    테스트 이름 표시하기

--- src/test/java/com/example/junit/basic/chapter_01/StudyTest.java
+++ src/test/java/com/example/junit/basic/chapter_01/StudyTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// 이거는 언더 스코어를 공백을 바꿔준다 ( 메서드 이름 )
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     /*
@@ -12,6 +14,17 @@ class StudyTest {
     @BeforeEach : 각각의 테스트가 실행되기 전에 실행된다.
     @AfterEach : 각각의 테스트가 실행된 후에 실행된다.
     @Disabled : 테스트를 실행하지 않는다.
+
+    Junit5 부터는 @BeforeClass, @AfterClass, @Before, @After 어노테이션을 사용하지 않는다.
+    Junit 테스트 이름 표시하기
+
+    @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+    - 메서드와 클래스 래퍼런스를 사용해서 테스트 이름을 표기하는 방법을 설정한다.
+    - 기본 구현체로 ReplaceUnderscores 구현체를 제공한다.
+
+    @DisplayName
+    - 메서드 별로 테스트 이름을 표기하는 방법을 설정한다.
+    - @DisplayNameGeneration 보다 우선순위가 높다.
      */
 
     @BeforeAll
@@ -32,6 +45,14 @@ class StudyTest {
     @AfterEach
     void afterEach() {
         System.out.println("afterEach");
+    }
+
+    /*
+    test 메서드 이름을 표시하는 방법
+     */
+    @Test
+    void displayName() {
+        System.out.println("displayName");
     }
 
     @Test
